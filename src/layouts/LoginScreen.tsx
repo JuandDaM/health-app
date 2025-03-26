5a8cf5c23f298f6f95abf09c3ed6f92ca434f7f4
JuandDaM <juandavidmartinezcastillo5@gmail.com>
2025-03-25 21:59:21 -0500

    Update the login

---
+++ src/layouts/LoginScreen.tsx
@@ -0,0 +1,21 @@
+import { View, Text, TextInput, } from "react-native";
+
+export default function LoginScreen() {
+    return (
+
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text> Login.</Text>
+            <Text> E-mail:</Text>
+            <TextInput placeholder="Ingresa el dato: "></TextInput>
+            <Text>Contraseña:</Text>
+            <TextInput  keyboardType="visible-password" placeholder="Ingresa la contraseña: ">
+            </TextInput>
+        </View>
+    );
+}
