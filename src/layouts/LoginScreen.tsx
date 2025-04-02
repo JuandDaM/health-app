c99124b316566efc1de966affa47a4b0c4e78e9c
JuandDaM <juandavidmartinezcastillo5@gmail.com>
2025-04-01 21:52:49 -0500

    Update Project

--- src/layouts/LoginScreen.tsx
+++ src/layouts/LoginScreen.tsx
@@ -1,21 +1,74 @@
-import { View, Text, TextInput, } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput, StyleSheet, Alert, TouchableOpacity } from "react-native";
+import { useEffect } from "react";
+import { useNavigation } from "expo-router";
+import { Header } from "react-native/Libraries/NewAppScreen";
+import RegisterScreen from "./RegisterScreen";
 
 export default function LoginScreen() {
-    return (
+    const navigation = useNavigation();
+    const [showRegister, setShowRegister] = useState(false);
 
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text> Login.</Text>
-            <Text> E-mail:</Text>
-            <TextInput placeholder="Ingresa el dato: "></TextInput>
-            <Text>Contraseña:</Text>
-            <TextInput   placeholder="Ingresa la contraseña: ">
-            </TextInput>
-        </View>
+    useEffect(() => {
+        navigation.setOptions({ headerShown: false });
+    }, [navigation]);
+
+    if (showRegister) {
+        return <RegisterScreen onClose={() => setShowRegister(false)}/>
+    }
+
+    return (<View
+        style={styles.container}>
+        <Text style={styles.title}>Sign in</Text>
+        <TextInput style={styles.input} placeholder="Admin@gmail.com" />
+        <TextInput style={styles.input} placeholder="*****************" secureTextEntry />
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowRegister(true)}>
+            <Text style={styles.link}>Sign up</Text>
+        </TouchableOpacity>
+    </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F0F0F0"
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 20
+    },
+    input: {
+        width: "80%",
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: "while",
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "#ccc"
+    },
+    button: {
+        marginTop: 20,
+        backgroundColor: "#007bff",
+        padding: 10,
+        borderRadius: 5,
+        width: "80%",
+        alignItems: "center"
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    link: {
+        marginTop: 10,
+        color: "blue",
+        textDecorationLine: "underline"
+    },
+});
---
+++ src/layouts/RegisterScreen.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import {View, Text,TextInput,TouchableOpacity,StyleSheet} from "react-native";
+
+export default function RegisterScreen({onClose}:any){
+    return (
+        <View>
+            <Text>Sign up</Text>
+            <TouchableOpacity onPress={onClose}>
+                <Text>Go to Sign in</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
