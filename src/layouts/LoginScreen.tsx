b519c49e41278168e02f57845d6cf5c834ec4855
luis obando <luis@gmail.com>
2025-04-03 21:56:43 -0500

    Register, import supabase,update project

--- src/layouts/LoginScreen.tsx
+++ src/layouts/LoginScreen.tsx
@@ -1,74 +1,85 @@
 import { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Alert, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 import { useEffect } from "react";
 import { useNavigation } from "expo-router";
 import { Header } from "react-native/Libraries/NewAppScreen";
 import RegisterScreen from "./RegisterScreen";
 
 export default function LoginScreen() {
-    const navigation = useNavigation();
-    const [showRegister, setShowRegister] = useState(false);
+  const navigation = useNavigation();
+  const [showRegister, setShowRegister] = useState(false);
 
-    useEffect(() => {
-        navigation.setOptions({ headerShown: false });
-    }, [navigation]);
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
 
-    if (showRegister) {
-        return <RegisterScreen onClose={() => setShowRegister(false)}/>
-    }
+  if (showRegister) {
+    return <RegisterScreen onClose={() => setShowRegister(false)} />;
+  }
 
-    return (<View
-        style={styles.container}>
-        <Text style={styles.title}>Sign in</Text>
-        <TextInput style={styles.input} placeholder="Admin@gmail.com" />
-        <TextInput style={styles.input} placeholder="*****************" secureTextEntry />
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Sign in</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => setShowRegister(true)}>
-            <Text style={styles.link}>Sign up</Text>
-        </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign in</Text>
+      <TextInput style={styles.input} placeholder="Admin@gmail.com" />
+      <TextInput
+        style={styles.input}
+        placeholder="*****************"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setShowRegister(true)}>
+        <Text style={styles.link}>Sign up</Text>
+      </TouchableOpacity>
     </View>
-    );
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F0F0F0"
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        marginBottom: 20
-    },
-    input: {
-        width: "80%",
-        padding: 10,
-        marginVertical: 10,
-        backgroundColor: "while",
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: "#ccc"
-    },
-    button: {
-        marginTop: 20,
-        backgroundColor: "#007bff",
-        padding: 10,
-        borderRadius: 5,
-        width: "80%",
-        alignItems: "center"
-    },
-    buttonText: {
-        color: "white",
-        fontSize: 18,
-        fontWeight: "bold"
-    },
-    link: {
-        marginTop: 10,
-        color: "blue",
-        textDecorationLine: "underline"
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F0F0F0",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "80%",
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "while",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#007bff",
+    padding: 10,
+    borderRadius: 5,
+    width: "80%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 10,
+    color: "blue",
+    textDecorationLine: "underline",
+  },
 });
--- src/layouts/RegisterScreen.tsx
+++ src/layouts/RegisterScreen.tsx
@@ -1,13 +1,68 @@
-import React, {useState} from "react";
-import {View, Text,TextInput,TouchableOpacity,StyleSheet} from "react-native";
+import React, { useState } from "react";
+import { supabase } from "../supabaseClient";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
-export default function RegisterScreen({onClose}:any){
-    return (
-        <View>
-            <Text>Sign up</Text>
-            <TouchableOpacity onPress={onClose}>
-                <Text>Go to Sign in</Text>
-            </TouchableOpacity>
-        </View>
-    );
-};
+export default function RegisterScreen({ onClose }: any) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign up</Text>
+      <TextInput style={styles.input} placeholder="Admin@gmail.com" />
+      <TextInput style={styles.input} placeholder="***************" />
+      <TextInput style={styles.input} placeholder="Your FullName" />
+      <TextInput style={styles.input} placeholder="(+57) 0000000000" />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onClose}>
+        <Text style={styles.link}>Go to Sign in</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F0F0F0",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "80%",
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "while",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#007bff",
+    padding: 10,
+    borderRadius: 5,
+    width: "80%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 10,
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+});
